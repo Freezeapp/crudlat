1af98e0e93ae2db1e99d069390e2d1da4346793f
yanekaputra <61059541+yanekaputra@users.noreply.github.com>
2022-09-25 22:48:19 +0800

    new file:   app/Models/Student.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php 	modified:   routes/web.php 	app/Http/Controllers/StudentController.php 	resources/views/example.blade.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//route untuk pemanggilan view
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route untuk pemanggilan kata "Hello World"
+route::get('/greeting', function(){
+    return 'hello world';
+}) -> name('greeting'); //pemberian nama pada route
+
+//route untuk pemanggilan kata yang di beri function
+route::get('/greeting/{name}', function($name){
+    return 'hello you' . " " . $name;
+}) -> name('greeting_with_name'); //pemberian nama pada route
